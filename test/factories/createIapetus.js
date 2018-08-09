b804b725318862983be88996418ccb291f494530
Gajus Kuizinas <gajus@gajus.com>
2018-08-09 23:45:21 +0100

    test: add basic integration test

--- test/factories/createIapetus.js
+++ test/factories/createIapetus.js
@@ -1,7 +1,15 @@
 // @flow
 
 import test from 'ava';
+import axios from 'axios';
+import createIapetus from '../../src/factories/createIapetus';
 
-test('OK', (t) => {
-  t.true(true);
+test('creates HTTP server on port 9050 with /metrics endpoint', async (t) => {
+  const iapetus = await createIapetus();
+
+  const response = await axios('http://127.0.0.1:9050/metrics');
+
+  t.true(response.data.includes('nodejs_version_info'));
+
+  await iapetus.stop();
 });
